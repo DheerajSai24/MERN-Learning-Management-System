60362e9ff4a9dde7ecd56b7e484d4a6970489d77
DheerajSai24 <dheerajsai628@gmail.com>
2025-08-30 21:51:41 +0530

    Update GitHub Actions to latest versions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/MERN-Learning-Management-System",
+  base:"/MERN-Learning-Management-System/",
 })
