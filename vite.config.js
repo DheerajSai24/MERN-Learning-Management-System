3d14b86cc1bf7d2b959402f9e9a608d590f1e415
DheerajSai24 <dheerajsai628@gmail.com>
2025-08-30 20:40:05 +0530

    deploy changes made

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/mern-learning-management-system"
+  base:"/MERN-Learning-Management-System"
+
 })
